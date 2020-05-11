0e720138c3b2b6ee27fa84b45dd6ce335699c3f3
Alexander Georgiev <alexfromsofia@gmail.com>
2020-05-11 20:32:59 +0300

    Set custom response

--- app.js
+++ app.js
@@ -3,5 +3,9 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/custom', (req, res) => {
+    res.status(418).set('x-alexfromsofia', 'Yes').send('Teapot!')
+    
+})
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
